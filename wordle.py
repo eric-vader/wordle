82955d03fdf82554e242274e60521f1ea6bbcc59
Han Liang Wee Eric <eric.aka.darth_vader@hotmail.com>
2022-02-17 11:24:27 +0800

    Updated new wordle

--- wordle.py
+++ wordle.py
@@ -1,5 +1,6 @@
 from collections import Counter, defaultdict
 import copy
+from itertools import product
 
 def is_wordpos_correct(ws, cs):
     for w, c in zip(ws, cs):
@@ -53,6 +54,69 @@ class Constraint(object):
 
         return ws
 
-ws = WordSpace("words_wordle.txt")
+    def count_filtered(self, ws):
+
+        diff_incorrect_set = self.incorrect_chars_set - set(self.correct_chars_set.keys())
+
+        count = 0
+        for w in ws.words:
+            if (is_wordpos_correct(w, self.correct_chars_list) 
+                    and all([ ws.freq_map_words[w][k] >= v for k,v in self.correct_chars_set.items() if not k in self.incorrect_chars_set ])
+                    and all([ ws.freq_map_words[w][k] == v for k,v in self.correct_chars_set.items() if k in self.incorrect_chars_set ])
+                    and (not any([u in diff_incorrect_set for u in set(ws.freq_map_words[w].keys())]))):
+                count += 1
+
+        return count
+
+class WordleEnv(object):
+    def __init__(self, target_word):
+        self.target_word = target_word
+    def guess(self, guess_word):
+        response = [0,0,0,0,0]
+        target_unique = set(self.target_word)
+        
+        for g_c, t_c, i in zip(guess_word, self.target_word, range(len(response))):
+            if g_c == t_c:
+                response[i] = 1
+                target_unique.discard(t_c)
+
+        for g_c, i in zip(guess_word, range(len(response))):
+            if g_c in target_unique:
+                response[i] = 2
+                target_unique.discard(g_c)
+
+        return ''.join(map(str, response))
+
+
+dict_ws = WordSpace("words_wordle.txt")
+ans_ws = WordSpace("words_wordle_ans.txt")
 c = Constraint("11111", "brawl")
-c.apply_to(ws)
+print(c.apply_to(dict_ws).words)
+
+# print(list(product(*[[1,2,3],[1,2,3],[1,2,3]])))
+responses_perm = [ ''.join(perm) for perm in product(*(["123"]*5)) ]
+from tqdm import tqdm
+from collections import defaultdict
+
+count_dict = {}
+for ea_guess_word in tqdm(dict_ws.words):
+    count_dict[ea_guess_word] = {}
+    for ea_dict_word in ans_ws.words:
+        env = WordleEnv(ea_dict_word)
+        ea_constraint = Constraint(env.guess(ea_guess_word), ea_guess_word)
+        count_dict[ea_guess_word][ea_dict_word] = ea_constraint.count_filtered(dict_ws)
+
+import pickle
+pickle.dump(count_dict, open('count_dict.pickle', 'wb'))
+
+
+
+# for ea_guess_word in dict_ws.words:
+#     total_filtered = 0
+#     for ea_response in responses_perm:
+#         total_filtered += Constraint(ea_response, ea_guess_word).count_filtered(dict_ws)
+#     print(len(dict_ws.words), total_filtered)
+    
+
+# env = WordleEnv("shake")
+# print(env.guess("sweet"))
